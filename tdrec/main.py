b3fc2662873d79316ea4ca173fa727b6a231863e
jun <1844250138@qq.com>
2025-02-21 18:00:54 +0800

    update.

---
+++ tdrec/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+def train_and_evaluate(
+        pipeline_config_path: str,
+        train_input_path: str,
+        eval_input_path: str,
+        model_dir: str = None,
+        continue_train: bool = True,
+) -> None:
+    pass
