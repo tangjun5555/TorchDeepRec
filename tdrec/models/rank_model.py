fdd1c65441402a96bb243ae047c04801a0145fa2
jun <1844250138@qq.com>
2025-02-21 17:58:20 +0800

    add rank model.

---
+++ tdrec/models/rank_model.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from typing import List, Any, Dict
+
+from tdrec.datasets.dataset import Batch
+from tdrec.protos.model_pb2 import ModelConfig
+from tdrec.features.feature import Feature
+from tdrec.models.base_model import BaseModel
+
+
+class RankModel(BaseModel):
+    def __init__(self,
+                 model_config: ModelConfig,
+                 features: List[Feature],
+                 labels: List[str],
+                 **kwargs: Any,
+                 ):
+        super().__init__(model_config, features, labels, **kwargs)
+
