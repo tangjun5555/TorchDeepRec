e665f8b3b70afcfddf84ad6bb3bcf18dff4f0327
jun <1844250138@qq.com>
2025-02-21 17:58:41 +0800

    add backbone module.

---
+++ tdrec/modules/backbone.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+class Backbone(object):
+    def __init(self,
+               config,
+               features,
+               input_layer,
+               l2_reg=None,
+               ):
+        pass
+
+
