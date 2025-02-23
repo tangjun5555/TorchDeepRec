5651da8edf438d8cc31f59a0201d36275928903f
jun <1844250138@qq.com>
2025-02-23 12:33:43 +0800

    add config util.

--- tdrec/utils/config_util.py
+++ tdrec/utils/config_util.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from tdrec.protos.pipeline_pb2 import PipelineConfig
+from google.protobuf import text_format
 
 
+def load_pipeline_config(pipeline_config_path: str, allow_unknown_field: bool = False) -> PipelineConfig:
+    config = PipelineConfig()
+    with open(pipeline_config_path) as f:
+        text_format.Merge(f.read(), config, allow_unknown_field=allow_unknown_field)
+    return config
