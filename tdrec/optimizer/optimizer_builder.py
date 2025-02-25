24488f2cd1f980934fc3e90740bf308087890ae7
jun <1844250138@qq.com>
2025-02-25 16:10:34 +0800

    update config util.

--- tdrec/optimizer/optimizer_builder.py
+++ tdrec/optimizer/optimizer_builder.py
@@ -2,12 +2,13 @@
 
 import torch
 from tdrec.protos.optimizer_pb2 import OptimizerConfig
+from tdrec.utils.config_util import config_to_kwargs
 
 
 def create_optimizer(optimizer_config: OptimizerConfig):
     optimizer_type = optimizer_config.WhichOneof("optimizer")
-    optimizer_config = getattr(optimizer_config, optimizer_type)
-    optimizer_kwargs = config_to_kwargs(oneof_optim_config)
+    oneof_optimizer_config = getattr(optimizer_config, optimizer_type)
+    optimizer_kwargs = config_to_kwargs(oneof_optimizer_config)
 
     if optimizer_type == "sgd_optimizer":
         return torch.optim.SGD, optimizer_kwargs
--- tdrec/utils/config_util.py
+++ tdrec/utils/config_util.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from typing import Dict, Any
+from google.protobuf import text_format, json_format
+from google.protobuf.message import Message
+
 from tdrec.protos.pipeline_pb2 import PipelineConfig
-from google.protobuf import text_format
 
 
 def load_pipeline_config(pipeline_config_path: str, allow_unknown_field: bool = False) -> PipelineConfig:
@@ -9,3 +12,12 @@ def load_pipeline_config(pipeline_config_path: str, allow_unknown_field: bool = 
     with open(pipeline_config_path) as f:
         text_format.Merge(f.read(), config, allow_unknown_field=allow_unknown_field)
     return config
+
+
+def config_to_kwargs(config: Message) -> Dict[str, Any]:
+    """Convert a message to a config dict."""
+    return json_format.MessageToDict(
+        config,
+        including_default_value_fields=True,
+        preserving_proto_field_name=True,
+    )
