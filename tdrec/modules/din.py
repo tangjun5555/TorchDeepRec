b6275869959e801a885c4152d9c14431334de744
jun <1844250138@qq.com>
2025-03-02 19:15:13 +0800

    add DIN module.

---
+++ tdrec/modules/din.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from typing import Dict, Any
+
+import torch
+from tdrec.modules.mlp import MLP
+
+
+@torch.fx.wrap
+def _arange(end: int, device: torch.device) -> torch.Tensor:
+    return torch.arange(end, device=device)
+
+
+class DIN(torch.nn.Module):
+    def __init__(self,
+                 sequence_dim: int,
+                 query_dim: int,
+                 feature_group: str,
+                 attn_mlp: Dict[str, Any],
+                 ) -> None:
+        super().__init__(input)
+        self._query_dim = query_dim
+        self._sequence_dim = sequence_dim
+        assert sequence_dim == sequence_dim
+
+        self.mlp = MLP(in_features=sequence_dim * 4, **attn_mlp)
+        self.linear = torch.nn.Linear(self.mlp.hidden_units[-1], 1)
+
+        self._query_name = f"{feature_group}.query"
+        self._sequence_name = f"{feature_group}.sequence"
+        self._sequence_length_name = f"{feature_group}.sequence_length"
+
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
+        query = inputs[self._query_name]
+        sequence = inputs[self._sequence_name]
+        sequence_length = inputs[self._sequence_length_name]
+        max_seq_length = sequence.size(1)
+        sequence_mask = _arange(
+            max_seq_length, device=sequence_length.device
+        ).unsqueeze(0) < sequence_length.unsqueeze(1)
+
+        queries = query.unsqueeze(1).expand(-1, max_seq_length, -1)
+
+        attn_input = torch.cat(
+            [queries, sequence, queries - sequence, queries * sequence],
+            dim=-1,
+        )
+        attn_output = self.mlp(attn_input)
+        attn_output = self.linear(attn_output)
+        attn_output = attn_output.transpose(1, 2)
+
+        padding = torch.ones_like(attn_output) * (-(2 ** 32) + 1)
+        scores = torch.where(sequence_mask.unsqueeze(1), attn_output, padding)
+        scores = torch.softmax(scores, dim=-1)
+        return torch.matmul(scores, sequence).squeeze(1)
