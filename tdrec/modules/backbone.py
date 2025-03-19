ee1dd6bae584be20848f2f08bc2c43b269e5ac2c
jun <1844250138@qq.com>
2025-03-19 19:40:50 +0800

    module backbone bugfix.

--- tdrec/modules/backbone.py
+++ tdrec/modules/backbone.py
@@ -3,6 +3,7 @@
 from typing import Dict, Any
 
 import torch
+from tdrec.utils.config_util import config_to_kwargs
 from tdrec.datasets.dataset import Batch
 from tdrec.features.feature_group import FeatureGroup
 from tdrec.protos.backbone_pb2 import BackboneConfig, BlockConfig
@@ -24,9 +25,11 @@ class Backbone(torch.nn.Module):
         feature_group_values = dict()
         for k, v in self._feature_group_dict.items():
             feature_group_values[k] = v.build_group_input(batch)
+        backbone_output = []
         for block in self._config.blocks:
-            self.build_block_output(block, feature_group_values)
-        backbone_output = torch.cat(list(self._block_outputs), dim=1)
+            block_output = self.build_block_output(block, feature_group_values)
+            backbone_output.append(block_output)
+        backbone_output = torch.cat(backbone_output, dim=1)
         return backbone_output
 
     def build_block_output(self, block_config: BlockConfig, feature_group_values: Dict[str, Any]) -> torch.Tensor:
@@ -37,11 +40,11 @@ class Backbone(torch.nn.Module):
         module_config = getattr(block_config, module_type)
         if module_type == "mlp":
             block_input = torch.cat(block_inputs, dim=1)
-            block_output = MLP(in_features=block_input.shape[1], **module_config)(block_input)
+            block_output = MLP(in_features=block_input.shape[1], **config_to_kwargs(module_config))(block_input)
         elif module_type == "din":
             block_input = block_inputs[0]
             sequence_dim = list(block_input.values())[0].shape[2]
-            block_output = DIN(sequence_dim=sequence_dim, query_dim=sequence_dim, **module_config)(block_input)
+            block_output = DIN(sequence_dim=sequence_dim, query_dim=sequence_dim, **config_to_kwargs(module_config))(block_input)
         else:
             raise ValueError(
                 f"block[{block_config.name}] don't support [{module_type}] now."
