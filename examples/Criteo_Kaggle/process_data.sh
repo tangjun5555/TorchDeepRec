682b99ad481056399e5a3cef9639919148bbe1c0
jun <1844250138@qq.com>
2025-04-05 17:24:08 +0800

    update Criteo_Kaggle example.

--- examples/Criteo_Kaggle/process_data.sh
+++ examples/Criteo_Kaggle/process_data.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # download data https://tianchi.aliyun.com/dataset/144733
-# save in {data_dir}, then split data.
-split -l 1000000 -d train.txt Criteo_Kaggle_split_
+# save in {data_dir}, then process data.
 
-python build_model_sample.py --raw_file=${data_dir}/Criteo_Kaggle_split_00 --res_file=${data_dir}/Criteo_Kaggle_split_00_T
-python build_model_sample.py --raw_file=${data_dir}/Criteo_Kaggle_split_01 --res_file=${data_dir}/Criteo_Kaggle_split_01_T
-python build_model_sample.py --raw_file=${data_dir}/Criteo_Kaggle_split_02 --res_file=${data_dir}/Criteo_Kaggle_split_02_T
-python build_model_sample.py --raw_file=${data_dir}/Criteo_Kaggle_split_03 --res_file=${data_dir}/Criteo_Kaggle_split_03_T
-python build_model_sample.py --raw_file=${data_dir}/Criteo_Kaggle_split_04 --res_file=${data_dir}/Criteo_Kaggle_split_04_T
+python build_model_sample.py --raw_file=${data_dir}/train.txt --res_file=${data_dir}/Criteo_Kaggle_T
+split -l 8000000 -d ${data_dir}/Criteo_Kaggle_T ${data_dir}/Criteo_Kaggle_T_
