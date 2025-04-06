903aa02ddcf788787822f07efad68b8042cb3f62
jun <1844250138@qq.com>
2025-04-06 14:07:40 +0800

    update Criteo_Kaggle example.

--- examples/Criteo_Kaggle/run_model_mlp.sh
+++ examples/Criteo_Kaggle/run_model_mlp.sh
@@ -3,14 +3,26 @@
 python -m tdrec.main \
   --task_type=train_and_evaluate \
   --pipeline_config_path=model_mlp.config \
-  --train_input_path=${data_dir}/Criteo_Kaggle_T_00,${data_dir}/Criteo_Kaggle_T_01 \
-  --eval_input_path=${data_dir}/Criteo_Kaggle_T_01
-# Mode.TRAIN Eval Result[model-31280]: auc:0.780717 copc:1.148334 ce_loss:0.545076
+  --train_input_path=${data_dir}/Criteo_Kaggle_T_00 \
+  --eval_input_path=${data_dir}/Criteo_Kaggle_T_00
+# Mode.TRAIN Eval Result[model-7826]: auc:0.777409 copc:0.960497 ce_loss:0.457538
 python -m tdrec.main \
   --task_type=evaluate \
   --pipeline_config_path=model_mlp.config \
   --eval_input_path=${data_dir}/Criteo_Kaggle_T_05
-# Mode.EVALUATE Eval Result[model-31280]: auc:0.774522 copc:1.162139 ce_loss:0.487913
+# Mode.EVALUATE Eval Result[model-7826]: auc:0.770061 copc:0.960418 ce_loss:0.484276
+
+python -m tdrec.main \
+  --task_type=train_and_evaluate \
+  --pipeline_config_path=model_mlp.config \
+  --train_input_path=${data_dir}/Criteo_Kaggle_T_01 \
+  --eval_input_path=${data_dir}/Criteo_Kaggle_T_01
+# Mode.TRAIN Eval Result[model-15654]: auc:0.777881 copc:1.181039 ce_loss:0.536273
+python -m tdrec.main \
+  --task_type=evaluate \
+  --pipeline_config_path=model_mlp.config \
+  --eval_input_path=${data_dir}/Criteo_Kaggle_T_05
+# Mode.EVALUATE Eval Result[model-15654]: auc:0.771365 copc:1.196604 ce_loss:0.498744
 
 python -m tdrec.main \
   --task_type=export \
